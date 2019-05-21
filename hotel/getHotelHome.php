93c785d7949f6889807a13f79075686352d2f1d9
Tran T. Ngoc <ttngoc653@gmail.com>
2019-05-22 00:03:06 +0700

    add limit in get hotels in home

--- hotel/getHotelHome.php
+++ hotel/getHotelHome.php
@@ -1,20 +1,33 @@
 <?php 
 
     require("./../helper/checkToken.php");
-    if(empty($_POST['latitude']) || empty($_POST['longitude']))
+    if(empty($_POST['latitude']) || empty($_POST['longitude']) || empty($_POST['radius']))
     {
-        die();
         http_response_code(401);
+        die();
     }
 
     require("./../helper/connect_db.php");
     $Database = new myDatabase();
     $db = $Database->connect();
 
-    
-    $query = $db->prepare("SELECT * FROM `hotel` ORDER BY SQRT((latitude - :latitude)*(latitude - :latitude)+(longitude - :longitude)*(longitude - :longitude))");
+    $limit="";
+
+    $checkNumberLimit = isset($_POST['limitfrom']) && isset($_POST['limitcount']) && filter_var($_POST['limitfrom'], FILTER_VALIDATE_INT) && filter_var($_POST['limitcount'], FILTER_VALIDATE_INT)
+
+    if ($checkNumberLimit) {
+        $limit=" LIMIT :limitFrom , :limitTo";
+    }
+
+    $query = $db->prepare("SELECT * FROM `hotel` WHERE SQRT((latitude - :latitude)*(latitude - :latitude)+(longitude - :longitude)*(longitude - :longitude)) <= :radius ORDER BY SQRT((latitude - :latitude)*(latitude - :latitude)+(longitude - :longitude)*(longitude - :longitude)) ".$limit);
     $query->bindParam("latitude", $_POST['latitude']);
     $query->bindParam("longitude", $_POST['longitude']);
+    $query->bindParam("radius", $_POST['radius']);
+
+    if ($checkNumberLimit) {
+        $query->bindParam("limitFrom", $_POST['limitfrom']);
+        $query->bindParam("limitTo", $_POST['limitcount']);
+    }
 
     $query->execute();
 
@@ -23,5 +36,4 @@
     $query->clodeCursor();
 
     http_response_code(200);
-
  ?>         
