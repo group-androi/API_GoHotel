840cf48efb712815ca96fb54af2f214d715db2c8
Tran T. Ngoc <ttngoc653@gmail.com>
2019-05-22 00:04:37 +0700

    set attribute fetch result json colunm name

--- helper/connect_db.php
+++ helper/connect_db.php
@@ -15,6 +15,7 @@
       try { 
         $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name . ";charset=UTF8", $this->username, $this->password);
         $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
       } catch(PDOException $e) {
         echo 'Connection Error: ' . $e->getMessage();
       }
