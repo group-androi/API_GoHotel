4b18b84913e4beca0deae670a06fb7ea94ae76f8
Tran T. Ngoc <ttngoc653@gmail.com>
2019-05-23 00:09:53 +0700

    Update getHotelHome.php

--- hotel/getHotelHome.php
+++ hotel/getHotelHome.php
@@ -16,20 +16,14 @@
     $checkNumberLimit = isset($_POST['limitfrom']) && isset($_POST['limitcount']) && filter_var($_POST['limitfrom'], FILTER_VALIDATE_INT) && filter_var($_POST['limitcount'], FILTER_VALIDATE_INT);
 
     if ($checkNumberLimit) {
-        $limit=" LIMIT :limitFrom , :limitTo";
+        $limit=" LIMIT ".$_POST['limitcount'].", ".$_POST['limitcount'];
     }
 
     $query = $db->prepare("SELECT *,((latitude - :latitude)*(latitude - :latitude)+(longitude - :longitude)*(longitude - :longitude)) 'distance' FROM `hotel` ORDER BY ((latitude - :latitude)*(latitude - :latitude)+(longitude - :longitude)*(longitude - :longitude)) ".$limit);
     $query->bindParam("latitude", $_POST['latitude']);
     $query->bindParam("longitude", $_POST['longitude']);
 
-    if ($checkNumberLimit) {
-        $query->bindParam("limitFrom", $_POST['limitfrom']);
-        $query->bindParam("limitTo", $_POST['limitcount']);
-    }
-
     $query->execute();
-
     echo json_encode($query->fetchAll());
 
     $query->closeCursor();
