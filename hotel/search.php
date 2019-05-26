8bb19bad3809ce0ffc3ead2eaa0886513e654004
Tran T. Ngoc <ttngoc653@gmail.com>
2019-05-26 21:45:49 +0700

    fix bug search

--- hotel/search.php
+++ hotel/search.php
@@ -19,21 +19,20 @@ if (isset($_GET['key']) || isset($_POST['key'])) {
     $limitfrom=isset($_GET['limitfrom']) ? $_GET['limitfrom'] : isset($_POST['limitfrom']) ? $_POST['limitfrom'] : '';
     $limitcount=isset($_GET['limitcount']) ? $_GET['limitcount'] : isset($_POST['limitcount']) ? $_POST['limitcount'] : '';
 
-    $checkNumberLimit = isset($limitfrom) && isset($limitcount) && NULL!=filter_var($limitfrom, FILTER_VALIDATE_INT) && NULL!=filter_var($limitcount, FILTER_VALIDATE_INT);
+    $checkNumberLimit = isset($limitfrom) && isset($limitcount) && is_numeric($limitfrom) && is_numeric($limitcount);
 
     if ($checkNumberLimit) {
         $limit=" LIMIT ".$limitfrom.", ".$limitcount;
     }
 
-    /*
-    echo $limit."<br/>";
+    /*echo $limit."<br/>";
     echo isset($_POST['limitfrom']).'<br/>';
     echo isset($_POST['limitcount']).'<br/>';
-    echo filter_var($_POST['limitfrom'], FILTER_VALIDATE_INT).'<br/>';
-    echo filter_var($_POST['limitcount'], FILTER_VALIDATE_INT).'<br/>';
+    echo $checkNumberLimit.'<br/>';
+    echo is_numeric($_POST['limitfrom']).'<br/>';
+    echo is_numeric($_POST['limitcount']).'<br/>';
+    echo "SELECT * FROM `hotel`left join `image` on `image`.`hotel_id` = `hotel`.`id_hotel` WHERE name_hotel like \'".$key_search."\' ".$limit.'<br/>';
 */
-    //echo "SELECT * FROM `hotel`left join `image` on `image`.`hotel_id` = `hotel`.`id_hotel` WHERE name_hotel like \'".$key_search."\' ".$limit.'<br/>';
-
     $query = $db->prepare("SELECT * FROM `hotel`left join `image` on `image`.`hotel_id` = `hotel`.`id_hotel` WHERE name_hotel like :keyword ".$limit);
     $query->bindParam("keyword", $key_search);
     $query->execute();
