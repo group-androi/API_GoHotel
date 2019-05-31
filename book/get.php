47d86b3ebf64e25a6e1d3077452773b3aef0e8c0
Tran T. Ngoc <ttngoc653@gmail.com>
2019-05-31 22:34:43 +0700

    update get book with id

--- book/get.php
+++ book/get.php
@@ -6,14 +6,15 @@
 	$Database = new myDatabase();
 	$db = $Database->connect();
 
-	$where = (!empty($_GET['id'])) ? "WHERE id_book = :id" : "" ;
+	$where = (!empty($_GET['id']) || !empty($_POST['id'])) ? "WHERE id_book = :id" : "" ;
 
     $query = $db->prepare("SELECT *
                             FROM bookroom 
                             ".$where." 
                             ORDER BY date_start");
-    if (!empty($_GET["id"])) {
-    	$query->bindParam("id", $_GET['id']);
+    if (!empty($_GET["id"]) || !empty($_POST['id'])) {
+    	$key = isset($_GET['id']) ? $_GET['id'] : $_POST['id'];
+    	$query->bindParam("id", $key);
     }
     $query->execute();
     echo json_encode($query->fetchAll());
