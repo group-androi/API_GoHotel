5673f4d3f83c3294bf4222c22a9e1d301131e4fc
Tran T. Ngoc <ttngoc653@gmail.com>
2019-06-01 13:31:13 +0700

    Update getAccordingToToken.php

--- book/getAccordingToToken.php
+++ book/getAccordingToToken.php
@@ -5,7 +5,7 @@
 	$Database = new myDatabase();
 	$db = $Database->connect();
 
-    $query = $db->prepare("SELECT ELECT `id_book`, 
+    $query = $db->prepare("SELECT `id_book`, 
                             (SELECT name_hotel FROM hotel WHERE id_hotel = hotel_id) 'name_hotel', 
                             (SELECT name_room FROM room WHERE id_room = room_id) 'name_room', 
                             `user_id`, `date_start`, `date_end`, `price`, `time_book`, `phone`, `info_user_booked`, `status` 
